5b4d7af2373744b74c12771bcef346e03f97c9c2
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2024-03-09 22:57:50 +0800

    Add files via upload

---
+++ T_250393_输出几个表达式.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout << "#include<iostream>" << endl;
+    cout << "using namespace std;" << endl;
+    cout << "int main() {" << endl;
+    cout << "   return 0;" << endl;
+    cout << "}";
+    return 0;
+}
---
+++ T_297740_输出_源代码.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout << 5/2 << endl;
+    cout << 5/2.0 << endl;
+    cout << 5.0/2;
+    return 0;
+}
